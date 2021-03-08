8b83d9c94300eee15d20b147517c7a144955e3b1
Nicolás Comerci <ncomerci@itba.edu.ar>
2021-03-08 11:30:37 -0300

    Update sokoban.py

--- sokoban.py
+++ sokoban.py
@@ -7,9 +7,9 @@ with open("config.json") as f:
     config = json.load(f)
 print(config)
 algorithm_list = ["BFS","DFS","IDDFS"]
-if config.get("algorithm") == None or config.get("algorihtm") not in algorithm_list:
-    print("ERROR: No algorithm provided or algorithm not supported. Config must contain an algorithm from the following list: [\"BFS\",\"DFS\",\"IDDFS\"]")
-else:
-    board = Board('map.txt')
-    
-    bfs(board)
+# if config.get("algorithm") == None or config.get("algorihtm") not in algorithm_list:
+#     print("ERROR: No algorithm provided or algorithm not supported. Config must contain an algorithm from the following list: [\"BFS\",\"DFS\",\"IDDFS\"]")
+# else:
+board = Board('maps/medium.txt')
+
+bfs(board)
