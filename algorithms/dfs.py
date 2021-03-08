94429edaeb5389c6dd59f808c0714e16f0e59b30
euge <epineiro@itba.edu.ar>
2021-03-08 19:48:08 -0300

    started dfs

---
+++ algorithms/dfs.py
@@ -0,0 +1,50 @@
+from node import Node
+from sokoban_render import render
+from algorithms.searchMethod import SearchMethod
+tree = {} # hash donde la clave es: 'posición del player + posiciones de las cajas' y el valor es lista de nodos
+
+
+class DFS(SearchMethod):
+    
+    def search(self,board):
+        node = Node(board.player, board.boxes, [])
+        self.queue.append(node)          #save initial node in stack
+        self.visited.append(node)
+        
+        while self.queue:  
+            curr = self.queue.pop() #it is a stack 
+            if(board.is_completed(curr)):
+                print(curr.steps) 
+                render(board.min_and_max[0], board.min_and_max[1], board.walls, board.boxes, board.goals, board.player, curr.steps)
+                return
+                    
+            moves = board.get_possible_moves(curr)
+          
+            for move in moves:
+                if move not in self.visited: 
+                    self.queue.append(move)
+                    self.visited.append(move)
+                    
+            
+    """ def search(self, board): 
+        node = Node(board.player, board.boxes, [])
+        visited = self.searchRec(board, node, self.visited)   
+        print(visited)            
+
+    def searchRec(self, board, node, visited): 
+        if node not in self.visited: 
+            self.visited.append(node)
+            
+            moves = board.get_possible_moves(node)
+            for move in moves:
+                searchRec(self, board, move, visited)
+        return visited  """            
+        
+                    
+                    
+    
+                    
+    
+
+            
+            
--- sokoban.py
+++ sokoban.py
@@ -1,6 +1,7 @@
 from board import Board
 import json
 from algorithms.bfs import BFS  
+from algorithms.dfs import DFS 
 #Aca parseamos el file de entrada y vamos llamando a cada algoritmo de busqueda
 
 with open("config.json") as f:
@@ -9,7 +10,10 @@ algorithm_list = ['BFS','DFS','IDDFS']
 if config.get('algorithm') == None or (config.get('algorithm') not in algorithm_list):
     print("ERROR: No algorithm provided or algorithm not supported. Config must contain an algorithm from the following list: [\"BFS\",\"DFS\",\"IDDFS\"]")
 else:
-    board = Board('maps/medium.txt')
+    board = Board('maps/map.txt')
     
-    bfs = BFS()
-    bfs.search(board)
+    #bfs = BFS()
+    #bfs.search(board)
+    
+    dfs = DFS()
+    dfs.search(board)
