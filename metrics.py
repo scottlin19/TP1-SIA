62d3758fadbcefa34ac486e2478fbd5103009c97
euge <epineiro@itba.edu.ar>
2021-03-09 11:13:18 -0300

    added metrics class

---
+++ metrics.py
@@ -0,0 +1,9 @@
+class Metrics:
+
+    def __init__(self, params, success, depth, cost, nodes_expanded, frontier):
+        self.params = params
+        self.success = success
+        self.depth = depth
+        self.cost =  cost
+        self.nodes_expanded = nodes_expanded
+        self.frontier = frontier
